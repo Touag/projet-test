c786baca6063ffede6bf79e69d789031317f7e59
Guillaume <podo.guillaume@gmail.com>
2022-09-19 16:53:48 +0200

    Calcule de Date pour reservation

---
+++ Date Calculator/index.js
@@ -0,0 +1,49 @@
+const today = new Date().toISOString().split('T')[0];
+start_date.value = today;
+start_date.min = today ;
+console.log(today);
+
+// tomorow
+
+let tomorrow = new Date () ;
+tomorrow.setDate(tomorrow.getDate()+1);
+
+// convert to inputformat
+
+let tomorrowFormat = tomorrow.toISOString().split('T')[0];
+
+end_date.value = tomorrowFormat
+end_date.min = tomorrowFormat
+
+
+start_date.addEventListener("change",(e)=>{
+let day = new Date ( e.target.value)
+if(end_date.value<start_date.value){
+    day.setDate(day.getDate()+1)
+    end_date.value = day.toISOString().split('T')[0];
+}
+
+});
+
+
+end_date.addEventListener('change',(e)=>{
+    let day = new Date (e.target.value);
+
+    if(end_date.value<start_date.value){
+     day.setDate(day.getDate()-1);
+     start_date.value= day.toISOString().split('T')[0];
+}});
+
+
+const bookingCalc =()=>{
+let diffTime = Math.abs(new Date (end_date.value)-new Date (start_date.value)
+);
+let diffDay = Math.ceil(diffTime/(1000*60*60*24));
+console.log(diffDay);
+totalPerNight.textContent = diffDay*nightPrice.textContent
+
+};
+
+
+start_date.addEventListener("change",bookingCalc);
+end_date.addEventListener("change",bookingCalc);
